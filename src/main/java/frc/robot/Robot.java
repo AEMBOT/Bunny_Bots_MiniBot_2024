7f9962944ab4d335ec663e4978e0ccc0ae3e894c
DustinTrumps <biggiraffeboy@outlook.com>
2024-12-04 12:15:06 -0800

    I did a few things -PWMSparkMax to CANSparkMax -made joystick use .getgetLeftX etc. instead of the "old joystick-only package" -added fan usage

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,53 +1,47 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Value;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
-/** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
-  private static final int kFrontLeftChannel = 2;
-  private static final int kRearLeftChannel = 3;
-  private static final int kFrontRightChannel = 1;
-  private static final int kRearRightChannel = 0;
-
-  private static final int kJoystickChannel = 0;
 
   private MecanumDrive m_robotDrive;
-  private Joystick m_stick;
+
+  private final CommandXboxController controller = new CommandXboxController(0);
+
+  private final Relay fan = new Relay(0);
 
   @Override
   public void robotInit() {
-    PWMSparkMax frontLeft = new PWMSparkMax(kFrontLeftChannel);
-    PWMSparkMax rearLeft = new PWMSparkMax(kRearLeftChannel);
-    PWMSparkMax frontRight = new PWMSparkMax(kFrontRightChannel);
-    PWMSparkMax rearRight = new PWMSparkMax(kRearRightChannel);
+    CANSparkMax frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+    CANSparkMax rearLeft = new CANSparkMax(2, MotorType.kBrushless);
+    CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
+    CANSparkMax rearRight = new CANSparkMax(4, MotorType.kBrushless);
 
     SendableRegistry.addChild(m_robotDrive, frontLeft);
     SendableRegistry.addChild(m_robotDrive, rearLeft);
     SendableRegistry.addChild(m_robotDrive, frontRight);
     SendableRegistry.addChild(m_robotDrive, rearRight);
 
-    // Invert the right side motors.
-    // You may need to change or remove this to match your robot.
     frontRight.setInverted(true);
     rearRight.setInverted(true);
 
     m_robotDrive = new MecanumDrive(frontLeft::set, rearLeft::set, frontRight::set, rearRight::set);
-
-    m_stick = new Joystick(kJoystickChannel);
+    // TODO: make joystick use .getgetLeftX etc. instead of the "old joystick-only package"
+    // m_stick = new Joystick(kJoystickChannel);
   }
 
   @Override
   public void teleopPeriodic() {
-    // Use the joystick Y axis for forward movement, X axis for lateral
-    // movement, and Z axis for rotation.
-    m_robotDrive.driveCartesian(-m_stick.getY(), -m_stick.getX(), -m_stick.getZ());
+    m_robotDrive.driveCartesian(-controller.getLeftY(), -controller.getLeftX(), -controller.getRightY());
+    controller.rightBumper().whileTrue(Commands.run(() -> fan.set(Value.kOn)));
+    controller.rightBumper().whileFalse(Commands.run(() -> fan.set(Value.kOff)));
   }
 }
