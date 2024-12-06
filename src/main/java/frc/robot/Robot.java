e068768608e38da22ab1ee615fb82c0b35737d0b
DustinTrumps <biggiraffeboy@outlook.com>
2024-12-06 13:06:48 -0800

    I did a few things: -changed controller.rightBumper() to controller.rightTrigger() because we are using a controller with button bumpers -inverted a few of the controls -fixed the get value axis for turning -condensed and added motor setup things we need to do: -wire up NAVX and fan -make field-centric instead of robot-centric with NAVX

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Relay.Value;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
@@ -18,30 +19,46 @@ public class Robot extends TimedRobot {
 
   private final Relay fan = new Relay(0);
 
+  CANSparkMax frontLeft = new CANSparkMax(2, MotorType.kBrushless);
+  CANSparkMax rearLeft = new CANSparkMax(3, MotorType.kBrushless);
+  CANSparkMax frontRight = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax rearRight = new CANSparkMax(4, MotorType.kBrushless);
+
+  private final CANSparkMax[] motors = {
+    frontLeft,
+    rearLeft,
+    frontRight,
+    rearRight,
+  };
+
   @Override
   public void robotInit() {
-    CANSparkMax frontLeft = new CANSparkMax(1, MotorType.kBrushless);
-    CANSparkMax rearLeft = new CANSparkMax(2, MotorType.kBrushless);
-    CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
-    CANSparkMax rearRight = new CANSparkMax(4, MotorType.kBrushless);
 
     SendableRegistry.addChild(m_robotDrive, frontLeft);
     SendableRegistry.addChild(m_robotDrive, rearLeft);
     SendableRegistry.addChild(m_robotDrive, frontRight);
     SendableRegistry.addChild(m_robotDrive, rearRight);
 
+    for (CANSparkMax motor : motors) {
+      motor.setIdleMode(CANSparkBase.IdleMode.kBrake);
+      motor.restoreFactoryDefaults();
+      motor.setCANTimeout(250);
+      motor.enableVoltageCompensation(12.0);
+      motor.setSmartCurrentLimit(25);
+      motor.burnFlash();
+    }
+    
+
     frontRight.setInverted(true);
     rearRight.setInverted(true);
 
     m_robotDrive = new MecanumDrive(frontLeft::set, rearLeft::set, frontRight::set, rearRight::set);
-    // TODO: make joystick use .getgetLeftX etc. instead of the "old joystick-only package"
-    // m_stick = new Joystick(kJoystickChannel);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.driveCartesian(-controller.getLeftY(), -controller.getLeftX(), -controller.getRightY());
-    controller.rightBumper().whileTrue(Commands.run(() -> fan.set(Value.kOn)));
-    controller.rightBumper().whileFalse(Commands.run(() -> fan.set(Value.kOff)));
+    m_robotDrive.driveCartesian(-controller.getLeftY(), controller.getLeftX(), controller.getRightX());
+    controller.rightTrigger().whileTrue(Commands.run(() -> fan.set(Value.kOn)));
+    controller.rightTrigger().whileFalse(Commands.run(() -> fan.set(Value.kOff)));
   }
 }
